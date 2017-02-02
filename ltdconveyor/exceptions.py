c9fdede5e03ab10803c6094a44b24b5d6092c407
Jonathan Sick <jsick@lsst.org>
2017-02-02 10:23:49 -0700

    Add FastlyError based on ConveyorError
    The FastlyError exception will be used for errors with Fastly API usage.
    
    Now all exceptions, including the existing S3Error will have
    ConveyorError as a common base class, enabling applications to trap any
    application exception raise by ltdconveyor.

--- ltdconveyor/exceptions.py
+++ ltdconveyor/exceptions.py
@@ -1,6 +1,16 @@
 """Exception library."""
 
 
-class S3Error(Exception):
-    """Errors related to AWS S3 usage."""
+class ConveyorError(Exception):
+    """Generic base exception class for ltdconveyor."""
+    pass
+
+
+class S3Error(ConveyorError):
+    """Error related to AWS S3 usage."""
+    pass
+
+
+class FastlyError(ConveyorError):
+    """Error related to Fastly API usage."""
     pass
