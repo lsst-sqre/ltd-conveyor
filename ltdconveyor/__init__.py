1d2f79cb38746baf42704b4dd5037b2af9e00769
Jonathan Sick <jsick@lsst.org>
2018-04-13 16:16:14 -0700

    Ignore __init__.py from flake8
    This is adopted practice since flake8 ignore comments aren't totally
    practical there.

--- ltdconveyor/__init__.py
+++ ltdconveyor/__init__.py
@@ -2,14 +2,14 @@ import logging
 
 from pkg_resources import get_distribution, DistributionNotFound
 
-from .s3utils import open_bucket  # noqa: F401
+from .s3utils import open_bucket
 from .s3upload import (upload_dir, upload_file, upload_object,  # noqa: F401
                        create_dir_redirect_object,  # noqa: F401
-                       ObjectManager)  # noqa: F401
-from .s3delete import delete_dir  # noqa: F401
-from .s3copy import copy_dir  # noqa: F401
-from .fastly import purge_key  # noqa: F401
-from .exceptions import S3Error, FastlyError, ConveyorError  # noqa: F401
+                       ObjectManager)
+from .s3delete import delete_dir
+from .s3copy import copy_dir
+from .fastly import purge_key
+from .exceptions import S3Error, FastlyError, ConveyorError
 
 # Allow applications to control the logging handler
 logging.getLogger(__name__).addHandler(logging.NullHandler())
