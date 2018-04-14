502ae9bad58d7452c8bc74286b7fa2e2ec6b6b95
Jonathan Sick <jsick@lsst.org>
2018-04-13 21:48:47 -0700

    Curate exported namespace
    This is particularly needed so that the Sphinx automodapi directive only
    captures relevant APIs, not any utilities imported into the __init__.py

--- ltdconveyor/__init__.py
+++ ltdconveyor/__init__.py
@@ -1,10 +1,16 @@
+__all__ = (
+    'open_bucket', 'upload_dir', 'upload_file', 'upload_object',
+    'create_dir_redirect_object', 'ObjectManager', 'delete_dir',
+    'copy_dir', 'purge_key', 'S3Error', 'FastlyError', 'ConveyorError'
+)
+
 import logging
 
 from pkg_resources import get_distribution, DistributionNotFound
 
 from .s3utils import open_bucket
-from .s3upload import (upload_dir, upload_file, upload_object,  # noqa: F401
-                       create_dir_redirect_object,  # noqa: F401
+from .s3upload import (upload_dir, upload_file, upload_object,
+                       create_dir_redirect_object,
                        ObjectManager)
 from .s3delete import delete_dir
 from .s3copy import copy_dir
