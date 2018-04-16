164f60bae043df4ac8b399d8af57c69cf6f312d3
Jonathan Sick <jsick@lsst.org>
2018-04-16 16:45:29 -0700

    Add --travis configuration to ltd upload
    The --travis option makes it easier to use run ltd upload from a travis
    environment by doing two things:
    
    - Auto-populating the git-refs from the Travis environment variables.
    
    - Allowing ltd upload to be an upload on certain types of Travis events.
      By default, uploads are disabled on PR envents, but this can be
      configured with --on-travis-pr flags, among other options.

--- ltdconveyor/cli/upload.py
+++ ltdconveyor/cli/upload.py
@@ -4,6 +4,8 @@
 __all__ = ('upload',)
 
 import logging
+import os
+import sys
 
 import click
 
@@ -20,9 +22,10 @@ from .utils import ensure_login
 )
 @click.option(
     '--git-ref',
-    required=True,
     help='Git ref, or space-delimited list of git refs. This versions the '
-         'build and helps LTD Keeper assign the build to an edition.'
+         'build and helps LTD Keeper assign the build to an edition. '
+         'Alternatively, version information can be auto-discovered by '
+         'setting --travis in a Travis CI job.'
 )
 @click.option(
     '--dir', 'dirname',
@@ -43,15 +46,54 @@ from .utils import ensure_login
     envvar='LTD_AWS_SECRET',
     help='AWS secret access key (or `$LTD_AWS_SECRET`).'
 )
+@click.option(
+    '--travis', 'ci_env', flag_value='travis',
+    help='Use environment variables from a Travis CI environment to set '
+         'the --git-ref option.'
+)
+@click.option(
+    '--on-travis-push/--no-travis-push', 'on_travis_push', default=True,
+    help='Upload on a Travis CI push (enabled by default). Must be used with '
+         '--travis.'
+)
+@click.option(
+    '--on-travis-pr/--no-travis-pr', 'on_travis_pr', default=False,
+    help='Upload on a Travis CI pull request event (disabled by default). '
+         'Must be used with --travis.'
+)
+@click.option(
+    '--on-travis-api/--no-travis-api', 'on_travis_api', default=True,
+    help='Upload on a Travis CI API event (enabled by default). '
+         'Must be used with --travis.'
+)
+@click.option(
+    '--on-travis-cron/--no-travis-cron', 'on_travis_cron', default=True,
+    help='Upload on a Travis CI cron event (enabled by default). '
+         'Must be used with --travis.'
+)
 @click.pass_context
-def upload(ctx, product, git_ref, dirname, aws_id, aws_secret):
-    """Upload a new build to LSST the Docs.
+def upload(ctx, product, git_ref, dirname, aws_id, aws_secret, ci_env,
+           on_travis_push, on_travis_pr, on_travis_api, on_travis_cron):
+    """Upload a new site build to LSST the Docs.
     """
     logger = logging.getLogger(__name__)
 
+    logger.debug('CI environment: %s', ci_env)
+    logging.debug('Travis events settings. '
+                  'On Push: %r, PR: %r, API: %r, Cron: %r',
+                  on_travis_push, on_travis_pr, on_travis_api, on_travis_cron)
+
+    # Abort upload on Travis CI under certain events
+    if ci_env == 'travis' and \
+            _should_skip_travis_event(
+                on_travis_push, on_travis_pr, on_travis_api, on_travis_cron):
+        sys.exit(0)
+
+    # Authenticate to LTD Keeper host
     ensure_login(ctx)
 
-    git_refs = git_ref.split(' ')
+    # Detect git refs
+    git_refs = _get_git_refs(ci_env, git_ref)
 
     build_resource = register_build(
         ctx.obj['keeper_hostname'],
@@ -83,3 +125,57 @@ def upload(ctx, product, git_ref, dirname, aws_id, aws_secret):
         ctx.obj['token']
     )
     logger.debug('Build %r complete', build_resource['self_url'])
+
+
+def _should_skip_travis_event(on_travis_push, on_travis_pr, on_travis_api,
+                              on_travis_cron):
+    """Detect if the upload should be skipped based on the
+    ``TRAVIS_EVENT_TYPE`` environment variable.
+
+    Returns
+    -------
+    should_skip : `bool`
+        True if the upload should be skipped based on the combination of
+        ``TRAVIS_EVENT_TYPE`` and user settings.
+    """
+    travis_event = os.getenv('TRAVIS_EVENT_TYPE')
+    if travis_event is None:
+        raise click.UsageError(
+            'Using --travis but the TRAVIS_EVENT_TYPE '
+            'environment variable is not detected.')
+
+    if travis_event == 'push' and on_travis_push is False:
+        click.echo('Skipping upload on Travis push event.')
+        return True
+    elif travis_event == 'pull_request' and on_travis_pr is False:
+        click.echo('Skipping upload on Travis pull request event.')
+        return True
+    elif travis_event == 'api' and on_travis_api is False:
+        click.echo('Skipping upload on Travis pull request event.')
+        return True
+    elif travis_event == 'cron' and on_travis_cron is False:
+        click.echo('Skipping upload on Travis cron event.')
+        return True
+    else:
+        return False
+
+
+def _get_git_refs(ci_env, user_git_ref):
+    if ci_env == 'travis' and user_git_ref is None:
+        # Get git refs from Travis environment
+        git_refs = _get_travis_git_refs()
+    elif user_git_ref is not None:
+        # Get git refs from command line
+        git_refs = user_git_ref.split()
+    else:
+        raise click.UsageError('--git-ref is required.')
+    return git_refs
+
+
+def _get_travis_git_refs():
+    git_refs = [os.getenv('TRAVIS_BRANCH')]
+    if git_refs[0] is None:
+        raise click.UsageError(
+            'Using --travis but the TRAVIS_BRANCH environment variable is '
+            'not detected.')
+    return git_refs
