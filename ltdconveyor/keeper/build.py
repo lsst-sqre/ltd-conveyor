11792d8387cd8d3eccf513ea1d6c2ae4e8735046
Jonathan Sick <jsick@lsst.org>
2019-06-24 15:21:25 -0400

    Use v2 endpoint for registering new build
    This endpoint includes post_url and post_fields data that allow clients
    to make uploads using presigned POST URLs.
    
    The function also takes a dirnames arg to let clients request presigned
    URLs for multple subdirectories within a site.

--- ltdconveyor/keeper/build.py
+++ ltdconveyor/keeper/build.py
@@ -3,6 +3,7 @@
 
 __all__ = ('register_build', 'confirm_build')
 
+import logging
 from urllib.parse import urljoin
 
 import requests
@@ -11,10 +12,10 @@ import uritemplate
 from .exceptions import KeeperError
 
 
-def register_build(host, keeper_token, product, git_refs):
+def register_build(host, keeper_token, product, git_refs, dirnames=None):
     """Register a new build for a product on LSST the Docs.
 
-    Wraps ``POST /products/{product}/builds/``.
+    Wraps the ``POST /products/{product}/builds/`` **v2 API** endpoint.
 
     Parameters
     ----------
@@ -27,6 +28,9 @@ def register_build(host, keeper_token, product, git_refs):
     git_refs : `list` of `str`
         List of Git refs that correspond to the version of the build. Git refs
         can be tags or branches.
+    dirnames : `list` of `str`
+        A list of relative directory names in the site. Each directory will
+        get its own presigned POST URL in the response from the LTD Keeper API.
 
     Returns
     -------
@@ -38,9 +42,13 @@ def register_build(host, keeper_token, product, git_refs):
     ltdconveyor.keeper.KeeperError
         Raised if there is an error communicating with the LTD Keeper API.
     """
+    logger = logging.getLogger(__name__)
+
     data = {
         'git_refs': git_refs
     }
+    if dirnames is not None:
+        data['directories'] = list(dirnames)
 
     endpoint_url = uritemplate.expand(
         urljoin(host, '/products/{p}/builds/'),
@@ -49,11 +57,17 @@ def register_build(host, keeper_token, product, git_refs):
     r = requests.post(
         endpoint_url,
         auth=(keeper_token, ''),
-        json=data)
+        json=data,
+        headers={'Accept': 'application/vnd.ltdkeeper.v2+json'}
+    )
 
     if r.status_code != 201:
         raise KeeperError(r.json())
     build_info = r.json()
+    logger.debug(
+        'Registered a build for product %s:\n%s',
+        product,
+        build_info)
     return build_info
 
 
