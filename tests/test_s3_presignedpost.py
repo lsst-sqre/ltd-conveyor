17630a604ad0d08ff695d85bd8e82d94629c3af9
Jonathan Sick <jsick@lsst.org>
2019-07-02 18:15:36 -0400

    Add test for prescan_directory

--- tests/test_s3_presignedpost.py
+++ tests/test_s3_presignedpost.py
@@ -1,11 +1,12 @@
 """Tests for the ``ltdconveyor.s3.presignedpost`` module.
 """
 
-from pathlib import PurePosixPath
+from pathlib import Path, PurePosixPath
 
 import pytest
 
-from ltdconveyor.s3.presignedpost import format_relative_dirname
+from ltdconveyor.s3.presignedpost import (
+    format_relative_dirname, prescan_directory)
 
 
 @pytest.mark.parametrize(
@@ -22,3 +23,18 @@ def test_relative_dirname(directory, basedir, expected):
     basedir = PurePosixPath(basedir)
     result = format_relative_dirname(directory, basedir)
     assert result == expected
+
+
+def test_prescan_directory():
+    """Test the prescan_directory function using a test directory in
+    ``tests/data/test-site/``
+    """
+    base_dir = Path(__file__).parent / 'data/test-site'
+    assert base_dir.is_dir()
+
+    dirs = prescan_directory(base_dir)
+    assert '/' in dirs
+    assert 'a/' in dirs
+    assert 'a/aa/' in dirs
+    assert 'b/' in dirs
+    assert len(dirs) == 4
