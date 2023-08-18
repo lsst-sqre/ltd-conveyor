ce8674d63fc68cdd1f0502d9ff80025c3ba97762
Jonathan Sick <jsick@lsst.org>
2023-08-18 13:59:24 -0400

    Drop support for Travis CI
    Drop the --travis option for ltd upload to enable metadata inference
    with Travis CI environments.

--- src/ltdconveyor/cli/upload.py
+++ src/ltdconveyor/cli/upload.py
@@ -39,7 +39,7 @@ __all__ = ["upload"]
     help="Git ref, or space-delimited list of git refs. This versions the "
     "build and helps LTD Keeper assign the build to an edition. "
     "Alternatively, version information can be auto-discovered by "
-    "setting --travis in a Travis CI job or --gh for GitHub actions.",
+    "setting --gh when running in a GitHub Actions environment.",
 )
 @click.option(
     "--gh",
@@ -47,41 +47,6 @@ __all__ = ["upload"]
     flag_value="gh",
     help="Use environment variables from a GitHub Actions environment to set "
     "the --git-ref option.",
-)
-@click.option(
-    "--travis",
-    "ci_env",
-    flag_value="travis",
-    help="Use environment variables from a Travis CI environment to set "
-    "the --git-ref option.",
-)
-@click.option(
-    "--on-travis-push/--no-travis-push",
-    "on_travis_push",
-    default=True,
-    help="Upload on a Travis CI push (enabled by default). Must be used with "
-    "--travis.",
-)
-@click.option(
-    "--on-travis-pr/--no-travis-pr",
-    "on_travis_pr",
-    default=False,
-    help="Upload on a Travis CI pull request event (disabled by default). "
-    "Must be used with --travis.",
-)
-@click.option(
-    "--on-travis-api/--no-travis-api",
-    "on_travis_api",
-    default=True,
-    help="Upload on a Travis CI API event (enabled by default). "
-    "Must be used with --travis.",
-)
-@click.option(
-    "--on-travis-cron/--no-travis-cron",
-    "on_travis_cron",
-    default=True,
-    help="Upload on a Travis CI cron event (enabled by default). "
-    "Must be used with --travis.",
 )
 @click.option(
     "--skip",
@@ -102,10 +67,6 @@ def upload(
     git_ref: Optional[str],
     dirname: str,
     ci_env: str,
-    on_travis_push: bool,
-    on_travis_pr: bool,
-    on_travis_api: bool,
-    on_travis_cron: bool,
     skip_upload: bool,
 ) -> None:
     """Upload a new site build to LSST the Docs."""
@@ -123,19 +84,6 @@ def upload(
         sys.exit(0)
 
     logger.debug("CI environment: %s", ci_env)
-    logger.debug(
-        "Travis events settings. " "On Push: %r, PR: %r, API: %r, Cron: %r",
-        on_travis_push,
-        on_travis_pr,
-        on_travis_api,
-        on_travis_cron,
-    )
-
-    # Abort upload on Travis CI under certain events
-    if ci_env == "travis" and _should_skip_travis_event(
-        on_travis_push, on_travis_pr, on_travis_api, on_travis_cron
-    ):
-        sys.exit(0)
 
     # Authenticate to LTD Keeper host
     ensure_login(ctx)
@@ -171,51 +119,10 @@ def upload(
         )
 
 
-def _should_skip_travis_event(
-    on_travis_push: bool,
-    on_travis_pr: bool,
-    on_travis_api: bool,
-    on_travis_cron: bool,
-) -> bool:
-    """Detect if the upload should be skipped based on the
-    ``TRAVIS_EVENT_TYPE`` environment variable.
-
-    Returns
-    -------
-    should_skip : `bool`
-        True if the upload should be skipped based on the combination of
-        ``TRAVIS_EVENT_TYPE`` and user settings.
-    """
-    travis_event = os.getenv("TRAVIS_EVENT_TYPE")
-    if travis_event is None:
-        raise click.UsageError(
-            "Using --travis but the TRAVIS_EVENT_TYPE "
-            "environment variable is not detected."
-        )
-
-    if travis_event == "push" and on_travis_push is False:
-        click.echo("Skipping upload on Travis push event.")
-        return True
-    elif travis_event == "pull_request" and on_travis_pr is False:
-        click.echo("Skipping upload on Travis pull request event.")
-        return True
-    elif travis_event == "api" and on_travis_api is False:
-        click.echo("Skipping upload on Travis pull request event.")
-        return True
-    elif travis_event == "cron" and on_travis_cron is False:
-        click.echo("Skipping upload on Travis cron event.")
-        return True
-    else:
-        return False
-
-
 def _get_git_refs(
     ci_env: Optional[str], user_git_ref: Optional[str]
 ) -> List[str]:
-    if ci_env == "travis" and user_git_ref is None:
-        # Get git refs from Travis environment
-        git_refs = _get_travis_git_refs()
-    elif ci_env == "gh" and user_git_ref is None:
+    if ci_env == "gh" and user_git_ref is None:
         # Get git refs from GitHub Actions environment
         git_refs = _get_gh_actions_git_refs()
     elif user_git_ref is not None:
@@ -223,16 +130,6 @@ def _get_git_refs(
         git_refs = user_git_ref.split()
     else:
         raise click.UsageError("--git-ref is required.")
-    return git_refs
-
-
-def _get_travis_git_refs() -> List[str]:
-    git_refs = [os.getenv("TRAVIS_BRANCH", "")]
-    if git_refs[0] == "":
-        raise click.UsageError(
-            "Using --travis but the TRAVIS_BRANCH environment variable is "
-            "not detected."
-        )
     return git_refs
 
 
--- tests/test_cli_upload.py
+++ tests/test_cli_upload.py
@@ -5,65 +5,7 @@ from typing import Any, List, Optional
 import click
 import pytest
 
-from ltdconveyor.cli.upload import (
-    _get_gh_actions_git_refs,
-    _get_git_refs,
-    _get_travis_git_refs,
-    _should_skip_travis_event,
-)
-
-
-@pytest.mark.parametrize(
-    "event_type,on_push,on_pr,on_api,on_cron,expected",
-    [
-        ("push", True, True, True, True, False),
-        ("push", False, True, True, True, True),
-        ("pull_request", True, True, True, True, False),
-        ("pull_request", True, False, True, True, True),
-        ("api", True, True, True, True, False),
-        ("api", True, True, False, True, True),
-        ("cron", True, True, True, True, False),
-        ("cron", True, True, True, False, True),
-    ],
-)
-def test_should_skip_travis_event(
-    monkeypatch: Any,
-    event_type: str,
-    on_push: bool,
-    on_pr: bool,
-    on_api: bool,
-    on_cron: bool,
-    expected: bool,
-) -> None:
-    """Test _should_skip_travis_event with different combinations of
-    user settings and TRAVIS_EVENT_TYPE variables.
-    """
-    import os
-
-    def mock_env_var(_: Any) -> str:
-        return event_type
-
-    monkeypatch.setattr(os, "getenv", mock_env_var)
-    assert (
-        _should_skip_travis_event(on_push, on_pr, on_api, on_cron) is expected
-    )
-
-
-def test_get_travis_git_refs(monkeypatch: Any) -> None:
-    import os
-
-    monkeypatch.setattr(os, "getenv", lambda *args: "my_branch")
-
-    assert _get_travis_git_refs() == ["my_branch"]
-
-
-def test_get_travis_git_refs_no_env_var(monkeypatch: Any) -> None:
-    import os
-
-    monkeypatch.setattr(os, "getenv", lambda *args: "")
-
-    with pytest.raises(click.UsageError):
-        _get_travis_git_refs()
+from ltdconveyor.cli.upload import _get_gh_actions_git_refs, _get_git_refs
 
 
 def test_get_gh_actions_git_refs(monkeypatch: Any) -> None:
@@ -86,11 +28,7 @@ def test_get_gh_actions_git_refs_no_env_var(monkeypatch: Any) -> None:
 @pytest.mark.parametrize(
     "env_var,ci_env,user_git_ref,expected",
     [
-        # using travis branch
-        ("my_branch", "travis", None, ["my_branch"]),
-        # overriding travis on command line
-        ("my_branch", "travis", "user-branch", ["user-branch"]),
-        # using GitHub actions branch
+        # using GitHub Actions branch
         ("refs/heads/my_branch", "gh", None, ["my_branch"]),
         ("refs/tags/my_tag", "gh", None, ["my_tag"]),
         ("refs/pull/my_pr", "gh", None, ["my_pr"]),
