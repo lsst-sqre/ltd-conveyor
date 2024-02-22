c66965f4ca6289c37251b9ae17be0b1ccbf5b5e3
Jonathan Sick <jsick@lsst.org>
2024-02-22 14:42:36 -0500

    Improve messages for LTD Keeper-related errors
    Provide a good error message and ensure that the readable text always
    appears at the bottom of the log.

--- src/ltdconveyor/keeper/exceptions.py
+++ src/ltdconveyor/keeper/exceptions.py
@@ -1,10 +1,23 @@
-"""Exceptions related to the LTD Keeper.
-"""
+"""Exceptions related to the LTD Keeper."""
 
 __all__ = ("KeeperError",)
+
+from typing import Optional
 
 from ..exceptions import ConveyorError
 
 
 class KeeperError(ConveyorError):
     """Error raised because of issues using the LTD Keeper API."""
+
+    def __init__(
+        self,
+        message: str,
+        status_code: Optional[int] = None,
+        body: Optional[str] = None,
+    ):
+        if status_code is not None:
+            message = f"(LTD status code: {status_code})\n\n{message}"
+        if body is not None:
+            message = f"{body}\n\n{message}"
+        super().__init__(message)
